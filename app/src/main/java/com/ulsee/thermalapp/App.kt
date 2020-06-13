d862d782cd7344c4351cc260686a407506f2e5f2
codus <codus.hsu@ulsee.com>
2020-06-13 16:38:44 +0800

    specific realm name, prevent migration crash

--- app/src/main/java/com/ulsee/thermalapp/App.kt
+++ app/src/main/java/com/ulsee/thermalapp/App.kt
@@ -2,13 +2,31 @@ package com.ulsee.thermalapp
 
 import android.app.Application
 import com.ulsee.thermalapp.data.Service
+import com.ulsee.thermalapp.data.model.RealmDevice
 import io.realm.Realm
+import io.realm.RealmConfiguration
+
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
+        val config = RealmConfiguration.Builder()
+            .name("appv1.realm")
+            .schemaVersion(1) // Must be bumped when the schema changes
+//            .migration { realm, oldVersion, newVersion ->
+//                if (oldVersion == 0L) {
+//                    val schema = realm.schema
+//                    schema.create("RealmDevice")
+//                        ?.addField("mID", String.javaClass)
+//                        ?.addField("mIP", String.javaClass)
+//                        ?.addField("mName", String.javaClass)
+//                        ?.addField("mCreatedAt", Long.javaClass)
+//                }
+//            } // Migration to run
+            .build()
+        Realm.setDefaultConfiguration(config)
         Service.shared.udpBroadcastService.initialize(applicationContext)
     }
 }
