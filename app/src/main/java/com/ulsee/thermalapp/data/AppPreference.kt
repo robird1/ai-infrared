85900ebeb9310a2caaca85e243e888d902843989
codus <codus.hsu@ulsee.com>
2020-06-17 13:50:47 +0800

    preference add notification enabled setting

--- app/src/main/java/com/ulsee/thermalapp/data/AppPreference.kt
+++ app/src/main/java/com/ulsee/thermalapp/data/AppPreference.kt
@@ -5,6 +5,7 @@ import android.content.SharedPreferences
 class AppPreference (prefs: SharedPreferences) {
 
     val PERMISSION_ONCE_CREATE_FIRST_DEVICE = "isOnceCreateFirstDevice"
+    val SETTINGS_FEVER_NOTIFICATION_ENABLED = "isFeverNotificationEnabled"
 
     private var sharedPreferences : SharedPreferences
 
@@ -18,6 +19,14 @@ class AppPreference (prefs: SharedPreferences) {
 
     fun setOnceCreateFirstDevice() {
         sharedPreferences.edit().putBoolean(PERMISSION_ONCE_CREATE_FIRST_DEVICE, true).apply()
+    }
+
+    fun isFeverNotificationEnabled(): Boolean {
+        return sharedPreferences.getBoolean(SETTINGS_FEVER_NOTIFICATION_ENABLED, false)
+    }
+
+    fun setIsFeverNotificationEnabled(enabled: Boolean) {
+        sharedPreferences.edit().putBoolean(SETTINGS_FEVER_NOTIFICATION_ENABLED, enabled).apply()
     }
 
 }
--- app/src/main/java/com/ulsee/thermalapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/ulsee/thermalapp/ui/settings/SettingsFragment.kt
@@ -1,15 +1,20 @@
 package com.ulsee.thermalapp.ui.settings
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
 import com.ulsee.thermalapp.MainActivity
 import com.ulsee.thermalapp.R
+import com.ulsee.thermalapp.data.AppPreference
 
 class SettingsFragment  : Fragment() {
+
+    lateinit var notificationSwitch: SwitchCompat
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +23,12 @@ class SettingsFragment  : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_settings, container, false)
         (activity as MainActivity).setTitle("Settings")
+
+        notificationSwitch = root.findViewById(R.id.switch_notification)
+        notificationSwitch.isChecked = AppPreference(requireContext().getSharedPreferences("app", Context.MODE_PRIVATE)).isFeverNotificationEnabled()
+        notificationSwitch.setOnCheckedChangeListener { _, isChecked ->
+            AppPreference(requireContext().getSharedPreferences("app", Context.MODE_PRIVATE)).setIsFeverNotificationEnabled(isChecked)
+        }
         return root
     }
 }
