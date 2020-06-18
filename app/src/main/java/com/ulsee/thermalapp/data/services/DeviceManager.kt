83cda8ffd27991f14965059372de78f2d79bc4a4
codus <codus.hsu@ulsee.com>
2020-06-18 15:27:55 +0800

    always show tutorial for just added device

--- app/src/main/java/com/ulsee/thermalapp/data/services/DeviceManager.kt
+++ app/src/main/java/com/ulsee/thermalapp/data/services/DeviceManager.kt
@@ -228,13 +228,14 @@ class DeviceManager(device: Device) {
                 val itemType = object : TypeToken<Settings>() {}.type
                 try {
                     settings = gson.fromJson<Settings>(responseString, itemType)
-                    if (settings?.IsFirstActivate == true) {
+                    //if (settings?.IsFirstActivate == true) {
                         val isJustJoinedDevice = Service.shared.justJoinedDeviceIDList.contains(device.getID())
                         Log.i(javaClass.name, "find first settings device: "+device.getID()+", is just joined:"+isJustJoinedDevice)
                         if (isJustJoinedDevice) {
                             Service.shared.requestTutorial(device.getID())
+                            Service.shared.justJoinedDeviceIDList.remove(device.getID())
                         }
-                    }
+                    //}
                 } catch(e: java.lang.Exception) {
                     Log.e(javaClass.name, "Error parse action "+action)
                     e.printStackTrace()
