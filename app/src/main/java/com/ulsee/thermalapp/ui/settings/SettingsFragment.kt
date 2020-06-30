710c7824d581cfc2e1e8212624641e1f57af786b
codus <codus.hsu@ulsee.com>
2020-06-30 11:31:07 +0800

    新增 設定頁面會顯示app版本

--- app/src/main/java/com/ulsee/thermalapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/ulsee/thermalapp/ui/settings/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.ulsee.thermalapp.ui.settings
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
@@ -29,6 +31,10 @@ class SettingsFragment  : Fragment() {
         notificationSwitch.setOnCheckedChangeListener { _, isChecked ->
             AppPreference(requireContext().getSharedPreferences("app", Context.MODE_PRIVATE)).setIsFeverNotificationEnabled(isChecked)
         }
+
+        val manager = context?.packageManager
+        val info = manager?.getPackageInfo(context?.packageName, PackageManager.GET_ACTIVITIES)
+        root.findViewById<TextView>(R.id.textView_version).text = "v "+info?.versionName
         return root
     }
 }
