4f235bce92202bec08f88ee05b357f31d0322b0b
steve.su <steve.su@ulsee.com>
2020-08-27 15:05:20 +0800

    1. switch to the correct hotspot when scaning QR code 2. display hotspot list and connect to the selected one

--- app/src/main/java/com/ulsee/thermalapp/ui/network/SettingsActivity.kt
+++ app/src/main/java/com/ulsee/thermalapp/ui/network/SettingsActivity.kt
@@ -13,7 +13,7 @@ import com.ulsee.thermalapp.data.Service
 import com.ulsee.thermalapp.data.model.WIFIInfo
 import com.ulsee.thermalapp.data.services.SettingsServiceTCP
 
-val TAG = "SettingsActivity"
+private val TAG = "SettingsActivity"
 
 const val MAX_TRY_CONNECT_TIMES = 30
 
@@ -98,8 +98,8 @@ class SettingsActivity : AppCompatActivity() {
     private fun switchAndSendACK() {
         val wifiInfo = intent.getSerializableExtra("wifi") as WIFIInfo
         var tryTimes = 0
-
-        if (connect(wifiInfo)) {
+        val result = NetworkUtils.connect(this, wifiInfo)
+        if (result) {
             val deviceManager = Service.shared.getManagerOfDeviceID(mDeviceID)
             Thread(Runnable {
                 while (tryTimes <= MAX_TRY_CONNECT_TIMES && !SettingsActivity@ this.isFinishing) {
@@ -176,48 +176,4 @@ class SettingsActivity : AppCompatActivity() {
         }
     }
 
-    fun connect (wifiInfo: WIFIInfo) : Boolean {
-        val networkSSID = wifiInfo.ssid
-        val networkPass = wifiInfo.password
-
-        val conf = WifiConfiguration()
-        conf.SSID = "\"" + networkSSID + "\""
-
-        val wifiType = wifiInfo.wifiType
-
-        when(wifiType) {
-            WIFIInfo.WIFIType.wep -> {
-                conf.wepKeys[0] = "\"" + networkPass + "\"";
-                conf.wepTxKeyIndex = 0;
-                conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-                conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
-            }
-            WIFIInfo.WIFIType.wpa -> {
-                conf.preSharedKey = "\""+ networkPass +"\"";
-            }
-            WIFIInfo.WIFIType.open -> {
-                conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-            }
-        }
-
-        val wifiManager = (getApplicationContext().getSystemService(Context.WIFI_SERVICE) as WifiManager)
-        wifiManager.addNetwork(conf)
-
-        val list: List<WifiConfiguration> = wifiManager.getConfiguredNetworks()
-        for (i in list) {
-            if (i.SSID != null && i.SSID == "\"" + networkSSID + "\"") {
-                wifiManager.disconnect()
-                wifiManager.enableNetwork(i.networkId, true)
-                val result = wifiManager.reconnect()
-                if (result) {
-                    return true
-                } else {
-                    Log.d(TAG, "[Before] setResult(Activity.RESULT_FIRST_USER) !!!!!!!!!!!!!!!!!!!!!!!!!!!!! ")
-                    setResult(Activity.RESULT_FIRST_USER)
-                }
-            }
-        }
-        Log.d(TAG, "wifiManager.getConfiguredNetworks() is empty.")
-        return false
-    }
 }
